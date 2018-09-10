697d32cadb5441f61dc2b46c38b0965c4e001bae
Joo Yong Chung <jdchung2@siebl-0224-27.ews.illinois.edu>
2018-09-09 20:03:27 -0500

    file submission

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -1,9 +1,11 @@
 #include <cstdlib>
 #include <cmath>
+#include <iostream>
 
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
 using namespace cs225;
+using namespace std;
 
 // sets up the output image
 PNG* setupOutput(unsigned w, unsigned h) {
@@ -13,37 +15,37 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
 // Returns my favorite color
 HSLAPixel* myFavoriteColor(double hue) {
-  HSLAPixel p(hue, 0.8, 0.5);
-  return &p;
+  HSLAPixel * p = new HSLAPixel (hue, 0.8, 0.5);
+  return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    PNG* original = new PNG();
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
-
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
-    HSLAPixel* myPixel = myFavoriteColor(-1);
+    HSLAPixel* myPixel = myFavoriteColor(220);
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; y < height; y++) {
+        for (unsigned x = 1; x < width; x++) {
             // Calculate the pixel difference
+          //  cout << "Reached line 42" << endl;
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
+          //  cout << "Reached line 45" << endl;
             double diff = std::fabs(curr.h - prev.h);
-
+            //cout << "Reached line 47" <<endl;
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel & currOutPixel = output -> getPixel(x, y);
+            //cout << "Reached line 51" <<endl;
             if (diff > 20) {
                 currOutPixel = *myPixel;
             }
@@ -54,7 +56,6 @@ void sketchify(std::string inputFile, std::string outputFile) {
     output->writeToFile(outputFile);
 
     // Clean up memory
-    delete myPixel;
     delete output;
     delete original;
 }
