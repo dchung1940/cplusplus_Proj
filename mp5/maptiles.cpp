d150cea1cfb18b76535dfc49de0a327b85e735b3
Daniel Chung <danielchung1940@gmail.com>
2018-11-05 17:41:12 -0600

    done

--- mp5/maptiles.cpp
+++ mp5/maptiles.cpp
@@ -21,7 +21,25 @@ MosaicCanvas* mapTiles(SourceImage const& theSource,
     /**
      * @todo Implement this function!
      */
-
-    return NULL;
+    MosaicCanvas * curr_image = new MosaicCanvas(theSource.getRows(),theSource.getColumns());
+    vector<Point<3>> vect_points;
+    map<Point<3>,TileImage> map_tiles; // dictionary for finding given tileImage
+    for(unsigned i=0; i< theTiles.size(); i++)
+    {
+      Point<3> point = convertToXYZ(theTiles[i].getAverageColor());
+      vect_points.push_back(point);
+      map_tiles.insert({point,theTiles[i]});
+    }
+    KDTree<3> kd(vect_points);
+    for(int i=0; i<theSource.getRows(); i++)
+    {
+      for(int j=0; j<theSource.getColumns(); j++)
+      {
+        Point<3> point = convertToXYZ(theSource.getRegionColor(i,j));
+        // kd.findNearestNeighbor(point);
+        TileImage temp_tile = map_tiles[kd.findNearestNeighbor(point)];
+        curr_image->setTile(i,j,&temp_tile);
+      }
+    }
+    return curr_image;
 }
-
